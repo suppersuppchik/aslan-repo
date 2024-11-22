b4cf904dbfe563849a6fc400c47dc8f4d0a22c4a
saba070201 <saba07022001@yandex.ru>
2024-11-22 17:33:26 +0300

    Add colorama in project

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-from .secret import SECRET_KEY 
-print(SECRET_KEY)
+from colorama import Fore, Back, Style
+print(Fore.RED + 'some red text')
+print(Back.GREEN + 'and with a green background')
+print(Style.DIM + 'and in dim text')
+print(Style.RESET_ALL)
+print('back to normal now')
